59522e6eb67bdc7bb7c2455fe6bdbd1aac86f23a
PC_User <PC_User@DESKTOP-BL8JU1M>
2023-04-28 15:38:58 +0900

    1.4　JAVAプログラミング

---
+++ 1.4JAVAプログラミング/src/lesson4/プログラミング.java
@@ -0,0 +1,77 @@
+package lesson4;
+
+public class プログラミング {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+   	 
+	     // Q1 下記9個をローカル変数として宣言のみしてください
+	     //		・バイト型・短整数型・整数型・長整数型
+	     // 	・単精度浮動小数点数型・倍精度浮動小数点数型
+	     //   	・文字型・文字列型
+	     //	 	・ブーリアン型 
+   	 	byte b;
+   	 	short c;
+   	 	int d;
+   	 	long e;
+   	 	float f;
+   	 	double g;
+   	 	char h;
+   	 	String i;
+   	 	boolean date;
+	     // Q2 それぞれのクラス変数をローカル内でそれぞれの初期値を代入し初期化してください
+   	 	b= 0;
+   	 	c= 0; 
+   	 	d = 0; 
+   	 	e = 0L; 
+   	 	f= 0.0f;
+   	 	g= 0.0d; 
+   	 	h = '\u0000'; 
+   	 	i = "こんにちは"; 
+   	 	date = false;
+	     // Q3 初期化をしたそれぞれの変数に下記の値を代入してください
+	     // ・バイト型                 10
+   	 	b = 10;
+	     // ・短整数型                 100
+   	 	c =100;
+	     // ・整数型                	 1000
+   	 	d =1000;
+	     // ・長整数型                 10000
+   	 	e = 10000;
+	     // ・単精度浮動小数点数型   	 9.5
+   	 	f = (float) 9.5;
+	     // ・倍精度浮動小数点数型		 10.5
+   	 	g = 10.5;
+	     // ・文字型                   a
+   	 	h = 'a';
+	     // ・文字列型              	 ハロー
+   	 	i = "ハロー";
+	     // ・ブーリアン型          	 true
+	    date = true;
+	     // Q4 下記の通りにコンソール出力されるようにしてください
+	     // 必ず変数を使用すること
+	     // 11110
+	    System.out.println(b + c + d + e);
+	     // 20.0
+	    System.out.println(f + g);
+	     // a ハロー true
+	    System.out.println(h + " " + i + " "+ date);
+	    // 11130.0                    数字を全て足す
+	    System.out.println(b + c + d + e + f + g);
+	     // 10000000000                小数点以外の数字を全てかける
+	    System.out.println(b * c * d * e);
+	     // 0.105                      10.5割る100をする
+	    System.out.println(g / c);
+	     // -90                        10引く100をする
+	    System.out.println(b - c);
+   	// Q5 
+   	// 次のプログラムを実行すると「ハローJAVA2023」という結果が表示されます。
+   	//「ハローJAVA43」と表示とさせたいのですが、意図通りに動きません。正しく動作するように修正してください。
+		String num = "4";
+		int num1 = 3;
+		System.out.println("ハローJAVA" + (num + num1));
+	}
+
+}
+
+    
